ca6ef1d733aeb7e95ff3ed9037ac5876d5eac5a3
prashant-lakshmanan <prashant.lakshmanan@gmail.com>
2024-10-28 16:05:58 +0530

    Update Hello.java

--- GitTest/src/test/java/testgit/Hello.java
+++ GitTest/src/test/java/testgit/Hello.java
@@ -3,7 +3,7 @@ package testgit;
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("Hello from Git");
+		System.out.println("Hello from Git first-change");
 
 	}
 
