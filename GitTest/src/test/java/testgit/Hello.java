220b600c8b7e7836b5c14e980ba4fd531a8f4b50
Prashant <prashant.lakshmanan@gmail.com>
2024-10-28 16:30:55 +0530

    conflict testing

--- GitTest/src/test/java/testgit/Hello.java
+++ GitTest/src/test/java/testgit/Hello.java
@@ -5,6 +5,7 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello from Git first-change");
 		System.out.println("Dssdssdsd---merge testing");
+		System.out.println("Merge testing---eclipse");
 
 	}
 
